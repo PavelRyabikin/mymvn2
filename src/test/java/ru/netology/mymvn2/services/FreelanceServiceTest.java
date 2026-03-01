b168ebfb24203510012c9a0344bc3de9d789cb06
Pavel Ryabikin <paulryabikin@gmail.com>
2026-03-01 15:21:35 +0300

    Add parameterized test

--- src/test/java/ru/netology/mymvn2/services/FreelanceServiceTest.java
+++ src/test/java/ru/netology/mymvn2/services/FreelanceServiceTest.java
@@ -1,36 +1,18 @@
 package ru.netology.mymvn2.services;
 
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 public class FreelanceServiceTest {
 
-    @Test
-    public void shouldCalculateThreeRestMonths() {
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/testdata.csv")
+    public void shouldCalculateThreeRestMonths(int income, int expenses, int threshold, int expected) {
         FreelanceService service = new FreelanceService();
 
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-
-        int expected = 3;
         int actual = service.calculate(income, expenses, threshold);
 
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void shouldCalculateTwoRestMonths() {
-        FreelanceService service = new FreelanceService();
-
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-
-        int expected = 2;
-        int actual = service.calculate(income, expenses, threshold);
-
-        assertEquals(expected, actual);
+        Assertions.assertEquals(expected, actual);
     }
 }
